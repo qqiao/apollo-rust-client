f6a514f90904c120bf8a9a01c7ebeefead3837c5
Qian Qiao <qian.qiao@gmail.com>
2025-07-13 14:52:42 +0800

    Fixed logging

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -fr /tmp/apollo && \
-RUST_LOG=apollo_rust_client::namespace=trace cargo test --lib -- --nocapture && \
+RUST_LOG=apollo_rust_client=trace cargo test --lib -- --nocapture && \
 RUST_BACKTRACE=1 wasm-pack test --node --lib -- --nocapture
