f89daae4881a992f02fac57185dcdeceb9326804
Qian Qiao <qian.qiao@gmail.com>
2025-08-20 16:36:10 +0800

    `clear` command doesn't work in GitHub

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-clear && rm -fr /tmp/apollo && \
+rm -fr /tmp/apollo && \
 RUST_LOG=apollo_rust_client=trace cargo test --lib -- --nocapture && \
 RUST_BACKTRACE=1 wasm-pack test --node --lib -- --nocapture
